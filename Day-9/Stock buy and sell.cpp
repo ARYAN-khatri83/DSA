a5f697954a0abd6ad0fabe30df860a77c447a5cc
Aryan khatri <120964113+ARYAN-khatri83@users.noreply.github.com>
2024-05-27 16:20:16 +0530

    Update Stock buy and sell.cpp

--- Day-9/Stock buy and sell.cpp
+++ Day-9/Stock buy and sell.cpp
@@ -1,3 +1,27 @@
+
+// BASIC APPROACH
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit = 0;
+        int n = prices.size();
+
+        for(int i = 0;i<n;i++){
+            for(int j = i+1 ; j<n;j++){
+                if(prices[j]>prices[i]){
+                    maxProfit = max(prices[j]-prices[i],maxProfit);
+                }
+            }
+            
+        }
+        return maxProfit;
+    }
+};
+
+// OPTIMAL
+
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
