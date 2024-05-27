0f73df4cc7ba5271518777383730033a8ed65f40
Aryan khatri <120964113+ARYAN-khatri83@users.noreply.github.com>
2024-05-27 15:20:43 +0530

    Update Two sum.cpp

--- Day-8/Array_mid_prob/Two sum.cpp
+++ Day-8/Array_mid_prob/Two sum.cpp
@@ -3,20 +3,49 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+
+       //  BASIC APPROACH TC = O(N2)
+        /* 
+        int n = nums.size();
+        for(int i = 0; i< n-1 ; i++){
+        for(int j = i+1 ;j<n;j++){
+         if(nums[i]+nums[j] == target;{
+         return {i,j};
+         }
+         }
+         }
+
+    */
+
+
+
+
+  // MEDIUM APPROACH  TC O(N) * LOG(N) SC = O(N)
         /*
-        map<int,int> mpp;
-        int n=nums.size();
-        for(int i =0;i<n;i++){
-            int a = nums[i];
-            int more = target-a;
-            if(mpp.find(more) != mpp.end()){
-                return {mpp[more],i};
+     class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> mpp; // declaration of map
+        int n = nums.size();
+        for(int i = 0 ;i<n ;i++){
+            int num = nums[i];
+            int more_nedded = target-num;
+            if(mpp.find(more_nedded) != mpp.end()){
+                return {mpp[more_nedded],i};
             }
-            mpp[a]=i;
+            mpp[num] = i;
         }
         return {};
-        */
 
+    }
+};
+
+
+*/
+
+
+        // OPTIMAL APPROACH
+// unordered map will increase the efficiency TC = O(n) * logn ,  SC = O(1)
         unordered_map<int,int> mpp;
         int n = nums.size();
         for(int i = 0 ;i<n;i++){
