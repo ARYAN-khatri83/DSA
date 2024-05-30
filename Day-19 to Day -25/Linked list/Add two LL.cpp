73c21f2e119b7550c803e5788bff731340c432de
Aryan khatri <120964113+ARYAN-khatri83@users.noreply.github.com>
2024-05-30 18:38:27 +0530

    Update Add two LL.cpp

--- Day-19 to Day -25/Linked list/Add two LL.cpp
+++ Day-19 to Day -25/Linked list/Add two LL.cpp
@@ -1,3 +1,21 @@
+
+Psuedocode
+1. Create a dummy node which is the head of new linked list.
+2. Create a node temp, initialise it with dummy.
+3. Initialize carry to 0.
+4. Loop through lists l1 and l2 until you reach both ends, and until carry is present.
+Set sum=l1.val+ l2.val + carry.
+Update carry=sum/10.
+Create a new node with the digit value of (sum%10) and set it to temp node's next, then advance temp node to next.
+Advance both l1 and l2.
+Return dummy's next node.
+Note that we use a dummy head to simplify the code. Without a dummy head, you would have to write extra conditional statements to initialize the head's value.
+
+Time Complexity: O(max(m,n)). Assume that m and n represent the length of l1 and l2 respectively, the algorithm above iterates at most max(m,n) times.
+
+Space Complexity: O(max(m,n)). The length of the new list is at most max(m,n)+1.
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -10,27 +28,36 @@
  */
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *dummy = new ListNode(); // create a dummy node
-        ListNode *temp = dummy; // copy the val of dummy
-        int carry=0;
-        while(l1 != NULL || l2 !=NULL || carry){
-            int sum = 0;
-            if(l1!=NULL){
-                sum+=l1->val; // if l1 is not null then we will add the val     
-                l1=l1->next;  // of l1 into the sum and inc the l1
-            }
-            if(l2!=NULL){
-                sum+=l2->val;
-                l2=l2->next;
-            }
-            sum+=carry; // adding carry to sum
-            carry = sum/10; // if sum is greate then 10 will div to find the dig
-            ListNode *node = new ListNode(sum%10); // create a new node for that new digit
-            temp->next = node; 
-            temp = temp->next;
-        }
-        return dummy->next;
-    }
+    ListNode* addTwoNumbers( ListNode* l1,  ListNode* l2) {
+        ListNode* dummyHead = new ListNode(-1); 
+        ListNode* temp = dummyHead;
+        ListNode* curr = dummyHead;
+        ListNode* temp1 = l1;
+        ListNode* temp2 = l2;
 
+        int carry  = 0;
+        while(temp1 !=  nullptr|| temp2 != nullptr){
+            int sum = carry;
+            if(temp1){
+                sum += temp1->val;
+                temp1 = temp1->next;
+            }
+            if(temp2){
+                 sum += temp2->val;
+                 temp2 = temp2->next;
+            }
+
+            ListNode* newNode = new ListNode(sum%10);
+            carry = sum/10;
+
+           curr->next = newNode;
+           curr =  curr->next;
+
+           if(carry>0){
+            // ListNode* newNode = new ListNode(carry);
+            curr->next = new ListNode(carry);
+           }
+        }
+        return dummyHead->next;
+    }
 };
