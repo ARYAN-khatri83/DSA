4aec76954530b207a70b9ba554f54b6ee42f96bb
Aryan khatri <120964113+ARYAN-khatri83@users.noreply.github.com>
2024-05-31 00:20:01 +0530

    Update odd even linked list.cpp

--- Day-19 to Day -25/Linked list/odd even linked list.cpp
+++ Day-19 to Day -25/Linked list/odd even linked list.cpp
@@ -11,22 +11,21 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if(!head || !head->next || !head->next->next) {
+        if(head == NULL || head->next == NULL){
             return head;
         }
-        ListNode *odd = head;
-        ListNode *even = head->next;
-        ListNode *evenstart = head->next;
 
-        while(odd->next && even->next) {
+        ListNode* odd = head;
+        ListNode* even = head->next; 
+        ListNode* evenHead = head->next; // evenHead would be used when there would be link change between odd and even , to point the last odd number to the even one.
+        while(even!=NULL && even->next!=NULL){ // even will ensure odd is completed 
             odd->next = even->next;
-            odd = odd->next;
-            even->next  = odd->next;;
-            even = even->next;
+            odd=odd->next;
+
+            even->next = odd->next;
+            even=even->next;
         }
-
-        odd->next = evenstart;
-        return head; 
-
+        odd->next = evenHead;
+        return head;
     }
 };
