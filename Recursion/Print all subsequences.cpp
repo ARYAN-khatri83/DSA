61fa4700d16be8a24c89a060deeb79882d2d8998
Aryan khatri <120964113+ARYAN-khatri83@users.noreply.github.com>
2024-06-04 11:29:32 +0530

    Update Print all subsequences.cpp

--- Recursion/Print all subsequences.cpp
+++ Recursion/Print all subsequences.cpp
@@ -4,16 +4,17 @@ using namespace std;
 void printF(int ind, vector<int> &ds, int arr[], int n) {
     if (ind == n) {
         for (auto it : ds) {
-            cout << it << " ";  // Added space between 'it' and '""'
+            cout << it << " "; 
         }
         cout << endl;
         return;
     }
 
-    ds.push_back(arr[ind]);
+    ds.push_back(arr[ind]);  // take  , these numbers are added into your subsequence
     printF(ind + 1, ds, arr, n);
     ds.pop_back();
 
+    // not take , these numbers are not added into your subsequence
     printF(ind + 1, ds, arr, n);
 }
 
