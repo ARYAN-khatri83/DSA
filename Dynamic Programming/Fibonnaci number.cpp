81c7dac1e4ab911074a6fbcb402cbe70cbea738a
Aryan khatri <120964113+ARYAN-khatri83@users.noreply.github.com>
2024-06-06 02:49:47 +0530

    Update Fibonnaci number.cpp

--- Dynamic Programming/Fibonnaci number.cpp
+++ Dynamic Programming/Fibonnaci number.cpp
@@ -30,3 +30,40 @@ private:
         return memo[n];
     }
 };
+
+// TABULATION METHOD
+class Solution {
+public:
+    int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        vector<int> dp(n + 1); // Use vector for dynamic memory allocation
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+};
+
+TC- O(N) SC - O(N) TO MAKE SC-O(1)
+    // SPACE OPTMIZATION 
+class Solution {
+public:
+    int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        int prev2 = 0;
+        int prev = 1;
+        for (int i = 2; i <= n; i++) {
+            int curr_i = prev + prev2;
+            prev2 = prev;
+            prev = curr_i;
+        }
+        return prev;
+    }
+};
+
