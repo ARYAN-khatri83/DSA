69ff8f8c19a840574a3e306ca65140b94a5124b2
Aryan khatri <120964113+ARYAN-khatri83@users.noreply.github.com>
2024-06-08 11:28:09 +0530

    Update Partition equal subset sum.cpp

--- Dynamic Programming/Partition equal subset sum.cpp
+++ Dynamic Programming/Partition equal subset sum.cpp
@@ -45,3 +45,44 @@ public:
         return subsetSumEqualsToK(n, target, nums);
     }
 };
+
+
+
+
+1. Subset Sum Function (subsetSumEqualsToK):
+This function checks if there is a subset of the given array arr that sums up to k.
+Parameters:
+n: Number of elements in the array.
+k: Target sum we are trying to find within subsets.
+arr: The array of integers.
+
+2. DP Array Initialization:
+dp is a 2D boolean array of size n x (k + 1) initialized to false. dp[i][j] will be true if a sum of j can be achieved using the first i elements.
+Base Case:
+dp[i][0] is set to true for all i because a sum of 0 can always be achieved by taking no elements.
+If the first element arr[0] is less than or equal to k, then dp[0][arr[0]] is set to true.
+
+3. DP Array Filling:
+Iterate over all elements from the second one onwards.
+For each element, iterate over all possible sums from 1 to k.
+For each position (index, target), check two possibilities:
+Not taking the current element: dp[index - 1][target]
+Taking the current element (if it's not greater than target): dp[index - 1][target - arr[index]]
+Combine these possibilities using the logical OR operator.
+
+4. Result:
+The value dp[n - 1][k] tells if it's possible to form a sum k using all n elements.
+
+5. Main Function (canPartition):
+This function determines if the array can be partitioned into two subsets with equal sum.
+Total Sum Calculation:
+Calculate the total sum of the array elements.
+Check for Odd Total Sum:
+If the total sum is odd, return false since it's impossible to split an odd number into two equal integer parts.
+
+6. Target Calculation:
+Set target as half of the total sum.
+Subset Sum Check:
+Use the helper function subsetSumEqualsToK to check if there exists a subset with sum equal to target.
+Return Result:
+Return the result from the helper function which determines if such a subset exists.
