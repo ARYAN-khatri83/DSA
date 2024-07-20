2687aaf24405e0a9e799dc5991051066d7d4c7f0
Aryan khatri <120964113+ARYAN-khatri83@users.noreply.github.com>
2024-07-20 13:05:58 +0530

    Delete Heaps  directory

--- Heaps /Kth largest element.cpp
+++
@@ -1,9 +0,0 @@
-// 1st Approach
-class Solution {
-public:
-    int findKthLargest(std::vector<int>& nums, int k) {
-        std::sort(nums.begin(), nums.end(), std::greater<int>());
-        return nums[k-1];
-    }
-};
-
